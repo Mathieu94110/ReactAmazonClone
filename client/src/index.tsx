067b203e2d948f20327e733239d0125d6f154905
mathieu enault <me94110@gmail.com>
2023-12-12 12:59:59 +0100

    test

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,6 +6,7 @@ import { router } from "./router";
 import "./assets/styles/index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
+console.log(process.env.REACT_APP_API_AUTH);
 root.render(
   <React.StrictMode>
     <RouterProvider router={router}></RouterProvider>
