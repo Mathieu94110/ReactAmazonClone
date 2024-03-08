963dffdcff61c37f2ca726c75efdda5262cab475
mathieu enault <me94110@gmail.com>
2024-03-08 19:26:17 +0100

    UserAd component styling improvement

--- client/src/pages/UserAdsPage/Components/UserAd/UserAd.tsx
+++ client/src/pages/UserAdsPage/Components/UserAd/UserAd.tsx
@@ -4,35 +4,69 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { Button, CardActionArea, CardActions } from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 export default function UserAd({ ad }) {
   return (
-    <Card sx={{ width: 260 }}>
+    <Card
+      sx={{
+        color: "white",
+        backgroundColor: "#131a22",
+        width: "260px",
+      }}
+    >
       <CardActionArea>
         <CardMedia
           component="img"
           height="140"
           image={ad.image}
-          alt="green iguana"
+          alt={ad.title}
+          sx={{
+            "&:hover": {
+              scale: "1.1",
+            },
+          }}
         />
         <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
+          <Typography
+            gutterBottom
+            variant="h5"
+            component="div"
+            sx={{
+              fontSize: 18,
+            }}
+          >
             {ad.title}
           </Typography>
-          <Typography gutterBottom variant="h5" component="div">
+          <Typography
+            gutterBottom
+            variant="h5"
+            component="div"
+            sx={{
+              fontSize: 16,
+            }}
+          >
             {ad.category}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {ad.description}
           </Typography>
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" color="primary">
-          Modifier
+        <Button>
+          <EditIcon
+            sx={{
+              color: "#f0c040",
+              "&:hover": { transform: "translateY(-4px);" },
+            }}
+          />
         </Button>
-        <Button size="small" color="primary">
-          Supprimer
+        <Button>
+          <DeleteIcon
+            sx={{
+              color: "#e74c3c",
+              "&:hover": { transform: "translateY(-4px);" },
+            }}
+          />
         </Button>
       </CardActions>
     </Card>
